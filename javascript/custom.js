f4d2258b50bf7f1a3f37cef9e145163edffb9c00
Andrei Inozemtsev <40149970+luckysle7in@users.noreply.github.com>
2018-06-13 15:57:41 +0200

    Material icons from Google, add questions types

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,77 +1,108 @@
-(function () {
-  var form = $("#example-form");
+$('.quick-answer-slider-item').on('click', function() {
+  $(this).toggleClass('checked');
+})
+  
+$('.rating-star').on('click', function() {
+  $('.rating-star').removeClass('star-on').removeClass('hover');
+  $('.rating-stars-title').removeClass('hover');
+  $('.rating-stars-title').text($(this).attr('data-title'));
+  $(this).parent().attr('data-title', $(this).attr('data-title'));
+  $(this).parent().attr('data-checked', $(this).index());
+  for (var i = 0; i <= $(this).index(); i++) {
+     $('.rating-star').eq(i).addClass('star-on');
+  }
+})	
 
-  form.validate({
+$('.rating-star').mouseover(function() {
+  $('.rating-star').removeClass('star-on').addClass('hover');
+  $('.rating-stars-title').addClass('hover');
+  $('.rating-stars-title').text($(this).attr('data-title'));
+  for (var i = 0; i <= $(this).index(); i++) {
+     $('.rating-star').eq(i).addClass('star-on');
+  }
+})	
 
-    errorPlacement: function errorPlacement(error, element) {
-      return true;
-    },
-
-    rules: {
-      phone: {
-        required: true
-      },
-      name: {
-        required: true
-      }
-    }
-  });
-
-  form.children("div").steps({
-    headerTag: "h3",
-    bodyTag: "section",
-    transitionEffect: "slideLeft",
-    labels: {
-      next: "Продолжить",
-      loading: "Загрузка ..."
-    },
-
-    onInit: function () {
-      form.find('.actions').after('<div class="steps-counter clearfix">шаг <span>1</span> из 5</div>')
-    },
-
-    onStepChanging: function (event, currentIndex, newIndex) {
+$('.rating-star').mouseout(function() {
+  $('.rating-star').removeClass('star-on').removeClass('hover');
+  $('.rating-stars-title').removeClass('hover');
+  $('.rating-stars-title').text($(this).parent().attr('data-title'));
+  for (var i = 0; i <= $(this).parent().attr('data-checked'); i++) {
+     $('.rating-star').eq(i).addClass('star-on');
+  }
+})	
 
 
-      if (currentIndex === 0) {
-        $('.wizard > .content').css('min-height', 320);
-      }
+$('.question-type-icons-item').on('click', function() {
+  $(this).parent().children('.question-type-icons-item').removeClass('active');
+  $(this).addClass('active');
+  $('.bottom-sticky a.button').removeClass('button-grey').addClass('button-green').children('.text').text('Next');
+})
 
-      if (form.valid() && currentIndex === 4 && newIndex === 5) {
 
-          $.post({
-            url:"/kitchen/kitchen-lab.php",
-            data:form.serialize(),
-            success: function() { },
-            error: function(jqXHR, textStatus, errorThrown) {
-              console.log('error', jqXHR, textStatus, errorThrown);
+$('.question-type-options-item').on('click', function() {
+  if ($(this).parent().hasClass('multiple')) {
+    $(this).toggleClass('active');
+  } else {
+    $(this).parent().children('.question-type-options-item').removeClass('active');
+    $(this).addClass('active');
+  }
+})
+
+
+
+
+$('#review_textarea').keyup(function(){
+  var text = '';
+  var icon = '';
+  var color = '';
+  var progressBar = document.querySelector('.progress-bar');
+
+  
+  if ($(this).val().length == 0) {
+    icon = '<i class="material-icons md-24">error_outline</i>';
+    text = "Recommended minimum 100 characters";
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-indicator').html(icon);
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-message').text(text);
+  } else if ($(this).val().length < 100) {
+    icon = '<div class="progress-bar"><svg viewPort="0 0 100 100"><circle class="progress-bar-element shadow"></circle><circle class="progress-bar-element progress"></circle></svg></div>';
+    text = $(this).val().length + ' / 100 minimun';
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-indicator').html(icon);
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-message').text(text);
+    progressBar = document.querySelector('.progress-bar');
+    progressBar.style.setProperty('--percent', $(this).val().length);
+  } else {
+    icon = '<i class="material-icons md-24">check_circle</i>';
+    text = "Good review! Enough...";
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-indicator').html(icon);
+    $(this).parent().children('.textarea-progress').children('.textarea-progress-message').text(text);
+  }
+  
+})
+
+
+
+
+
+
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
             }
-          });
-
-        $('.steps-counter').remove();
-      } else if (form.valid()) {
-        $('.steps-counter').find('span').html( parseInt(newIndex) + 1 );
-      }
-
-
-      return form.valid();
-    }
-  });
-
-  $('.header__apply').on('click', function (evt) {
-    evt.preventDefault();
-    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
-  });
-
-
-  var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: true
-  });
-
-}());
+        }
+    } catch (ex) {}
+}
